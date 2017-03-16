6d6f1ebc6ebc161538c83e696269949f95f8a65b
Adi Andrea <dekz.itfz@gmail.com>
2017-03-16 12:07:54 +0800

    impl add fungtion

--- app/src/main/java/dekz/traviscoveralls/MainActivity.java
+++ app/src/main/java/dekz/traviscoveralls/MainActivity.java
@@ -3,6 +3,7 @@ package dekz.traviscoveralls;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,12 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        final Calculator calculator = new Calculator();
 
         binding.btnTambah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "add", Toast.LENGTH_SHORT).show();
+                if(!TextUtils.isEmpty(binding.etNum1.getText())){
+                    binding.tvHasil.setText(String.valueOf(
+                            calculator.add(
+                                    Double.parseDouble(binding.etNum1.getText().toString()),
+                                    Double.parseDouble(binding.etNum2.getText().toString()))));
+                }
             }
         });
     }
