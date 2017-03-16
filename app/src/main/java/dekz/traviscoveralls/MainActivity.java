1300449f0bbebab62217585403931b02db5e7974
Adi Andrea <dekz.itfz@gmail.com>
2017-03-16 11:54:40 +0800

    enable data binding

--- app/src/main/java/dekz/traviscoveralls/MainActivity.java
+++ app/src/main/java/dekz/traviscoveralls/MainActivity.java
@@ -1,13 +1,29 @@
 package dekz.traviscoveralls;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import dekz.traviscoveralls.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        binding.btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "add", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
